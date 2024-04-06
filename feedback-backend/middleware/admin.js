3d4734a0ebabe0a2f74ef3d0228fa1afe823e78b
Tarique Hayat <tariqueht6@gmail.com>
2024-04-06 22:47:56 +0530

    handle admin middleware crash

--- feedback-backend/middleware/admin.js
+++ feedback-backend/middleware/admin.js
@@ -4,22 +4,27 @@ const Employee = require("../models/Employee");
 const SECRET_KEY = "1z`=TZ?fu12_";
 
 const authenticateAdmin = async (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (token == null) {
-    return res.sendStatus(401);
+  try {
+    const token = req.headers["authorization"];
+    if (token == null) {
+      return res.sendStatus(401);
+    }
+
+    const payload = jwt.verify(token, SECRET_KEY);
+
+    const user = await Employee.findById(payload.userId);
+
+    console.log(user);
+    if (user.role !== "admin") {
+      return res.sendStatus(401);
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500);
   }
-
-  const payload = jwt.verify(token, SECRET_KEY);
-
-  const user = await Employee.findById(payload.userId);
-
-  console.log(user);
-  if (user.role !== "admin") {
-    return res.sendStatus(401);
-  }
-
-  req.user = user;
-  next();
 };
 
 module.exports = authenticateAdmin;
